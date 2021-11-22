5f76e2fb0b0cbcf70fc137114b8e5cdea60d01ac
David <David.pralos@educa.jcyl.es>
2021-11-22 09:48:57 +0100

    Merge arreglado

---
+++ codigoPHP/ejercicio07PDOJSON.php
@@ -0,0 +1,75 @@
+<!DOCTYPE html>
+<html>
+    <head>
+        <meta charset="UTF-8">
+        <title>Ejercicio 07 PDO JSON</title>
+        <style>
+            a{
+                text-decoration: none;
+                color: grey;
+            }
+            h1{
+                text-align: center;
+            }
+        </style>
+    </head>
+    <body>
+        <?php
+            /*
+             * @author: David del Prado Losada
+             * @version: v1.Realizacion del ejercicio
+             * Created on: 16/11/2021
+             * Ejercicio 7.Página web que toma datos (código y descripción) de un fichero xml y los añade 
+             * a la tabla Departamento de nuestra base de datos. (IMPORTAR). El fichero importado se 
+             * encuentra en el directorio .../tmp/ del servidor.
+             */
+        
+            echo '<h1><a href=".."><=</a>   PROYECTO TEMA 4 - EJERCICIO 7</h1>';
+            
+            //Incluir el archivo de conexión con la base de datos
+            require_once "../config/confDBPDO.php";
+        
+            try{
+                //Conectar a la base de datos
+                $DAW2105DBDepartamentos = new PDO(HOST, USER, PASSWORD);
+                //Cambiar el atributo ERRMODE para que muestre la excepcion en caso de error
+                $DAW2105DBDepartamentos->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+                
+                //Query de inserccion de datos en la tabla Departamento
+                $consulta="INSERT INTO Departamento VALUES (:CodDepartamento, :DescDepartamento, :FechaBaja, :VolumenNegocio);";
+                $oResultado=$DAW2105DBDepartamentos->prepare($consulta);
+                
+                //Transforma el fichero a una cadena
+                $archivoJSON=file_get_contents("../tmp/departamento.json");
+                
+                //Transforma una cadena JSON a un formato que php pueda utilizar
+                $aDepartamentos=json_decode($archivoJSON);
+                
+                //Datos de cada departamento
+                foreach($aDepartamentos as $departamento){
+                    $oResultado->bindParam(':CodDepartamento', $departamento->CodDepartamento);
+                    $oResultado->bindParam(':DescDepartamento', $departamento->DescDepartamento);
+                    $oResultado->bindParam(':FechaBaja', $departamento->FechaBaja);
+                    $oResultado->bindParam(':VolumenNegocio', $departamento->VolumenNegocio);
+                    
+                    //Ejecutar el query
+                    $oResultado->execute();
+                }
+                
+                echo 'Datos importados';
+                
+            }catch(PDOException $excepcion){
+                $errorExcepcion=$excepcion->getCode();
+                $mensajeExcepcion=$excepcion->getMessage();
+                
+                //Mostrar el mensaje de la excepcion
+                echo '<p>Error: '.$mensajeExcepcion.'</p>';
+                //Mostrar el codigo de la excepcion
+                echo '<p>Codigo de error: '.$errorExcepcion.'</p>';
+            }finally{
+                //Cerrar conexión
+                unset($DAW2105DBDepartamentos);
+            }
+        ?>
+    </body>
+</html>
--- indexProyectoTema4.php
+++ indexProyectoTema4.php
@@ -1,4 +1,3 @@
-
 <!DOCTYPE html>
 <html>
     <head>
@@ -11,14 +10,16 @@
     <body>
         <h1>EJERCICIOS TEMA 4</h1>
         
-        <table>
+        <a href="mostrarcodigo/muestraConfiguracion.php"><h3>Configuración de la base de datos</h3></a>
+        
+        <table class="main">
             <tr>
                 <th>Enunciado</th>
                 <th colspan="2">PDO</th>
                 <th colspan="2">MySQLi</th>
             </tr>
             <tr>
-                <td>Ejercicio 01-Conexión a la base de datos con la cuenta usuario y tratamiento de errores</td>
+                <td>Ejercicio 01-Conexión a la base de datos con la cuenta usuario y tratamiento de errores.</td>
                 <td><a href="codigoPHP/ejercicio01PDO.php">Ejecutar</a></td>
                 <td><a href="mostrarcodigo/muestraEjercicio01PDO.php">Mostrar</a></td>
                 <td><a href="codigoPHP/ejercicio01MySQLi.php">Ejecutar</a></td>
@@ -26,7 +27,7 @@
                  
             </tr>
             <tr>
-                <td>Ejercicio 02-Mostrar el contenido de la tabla Departamento y el número de registros</td>
+                <td>Ejercicio 02-Mostrar el contenido de la tabla Departamento y el número de registros.</td>
                 <td><a href="codigoPHP/ejercicio02PDO.php">Ejecutar</a></td>
                 <td><a href="mostrarcodigo/muestraEjercicio02PDO.php">Mostrar</a></td>
                 <td><a>Ejecutar</a></td>
@@ -40,21 +41,21 @@
                 <td><a>Mostrar</a></td>
             </tr>
             <tr>
-                <td>Ejercicio 04-Formulario de búsqueda de departamentos por descripción(por una parte del campo DescDepartamento, si el usuario no pone nada deben aparecer todos los departamentos).</td>
+                <td>Ejercicio 04-Formulario de búsqueda de departamentos por descripción.</td>
                 <td><a href="codigoPHP/ejercicio04PDO.php">Ejecutar</a></td>
                 <td><a href="mostrarcodigo/muestraEjercicio04PDO.php">Mostrar</a></td>
                 <td><a>Ejecutar</a></td>
                 <td><a>Mostrar</a></td>
             </tr>
             <tr>
-                <td>Ejercicio 05-Pagina web que añade tres registros a nuestra tabla Departamento utilizando tres instrucciones insert y una transacción, de tal forma que se añadan los tres registros o no se añada ninguno</td>
+                <td>Ejercicio 05-Pagina web que añade tres registros a nuestra tabla Departamento utilizando tres instrucciones insert y una transacción, de tal forma que se añadan los tres registros o no se añada ninguno.</td>
                 <td><a href="codigoPHP/ejercicio05PDO.php">Ejecutar</a></td>
                 <td><a href="mostrarcodigo/muestraEjercicio05PDO.php">Mostrar</a></td>
                 <td><a>Ejecutar</a></td>
                 <td><a>Mostrar</a></td>
             </tr>
             <tr>
-                <td>Ejercicio 06-Pagina web que cargue registros en la tabla Departamento desde un array departamentosnuevos utilizando una consulta preparada</td>
+                <td>Ejercicio 06-Pagina web que cargue registros en la tabla Departamento desde un array departamentosnuevos utilizando una consulta preparada.</td>
                 <td><a href="codigoPHP/ejercicio06PDO.php">Ejecutar</a></td>
                 <td><a href="mostrarcodigo/muestraEjercicio06PDO.php">Mostrar</a></td>
                 <td><a>Ejecutar</a></td>
@@ -66,25 +67,29 @@
                 <th colspan="2">JSON</th>
             </tr>
             <tr>
-                <td>Ejercicio 07-Página web que toma datos (código y descripción) de un fichero xml y los añade a la tabla Departamento de nuestra base de datos. (IMPORTAR). El fichero importado se encuentra en el directorio .../tmp/ del servidor.</td>
+                <td>Ejercicio 07-Página web que toma datos de un fichero xml y los añade a la tabla Departamento de nuestra base de datos.</td>
                 <td><a href="codigoPHP/ejercicio07PDOXML.php">Ejecutar</a></td>
                 <td><a href="mostrarcodigo/muestraEjercicio07PDOXML.php">Mostrar</a></td>
-                <td><a>Ejecutar</a></td>
-                <td><a>Mostrar</a></td>
+                <td><a href="codigoPHP/ejercicio07PDOJSON.php">Ejecutar</a></td>
+                <td><a href="mostrarcodigo/muestraEjercicio07PDOJSON.php">Mostrar</a></td>
             </tr>
             <tr>
-                <td>Ejercicio 08-Página web que toma datos (código y descripción) de la tabla Departamento y guarda en un fichero departamento.xml. (COPIA DE SEGURIDAD / EXPORTAR). El fichero exportado se encuentra en el directorio .../tmp/ del servidor.</td>
+                <td>Ejercicio 08-Página web que toma datos de la tabla Departamento y guarda en un fichero departamento.xml.</td>
                 <td><a href="codigoPHP/ejercicio08PDOXML.php">Ejecutar</a></td>
                 <td><a href="mostrarcodigo/muestraEjercicio08PDOXML.php">Mostrar</a></td>
-                <td><a>Ejecutar</a></td>
-                <td><a>Mostrar</a></td>
+                <td><a href="codigoPHP/ejercicio08PDOJSON.php">Ejecutar</a></td>
+                <td><a href="mostrarcodigo/muestraEjercicio08PDOJSON.php">Mostrar</a></td>
+            </tr>
+            <tr>
+                <td>Ejercicio 09-Aplicación resumen MtoDeDepartamentosTema4.</td>
+                <td style="text-align: center;" colspan="4"><a href="../205DWESMtoDepartamentosTema4/codigoPHP/MtoDepartamentos.php">Ejecutar</a></td>
             </tr>
         </table>
         
         <footer>
             <table>
                 <tr>
-                    <td><p>David del Prado Losada - 03/11/2021 - DAW2</p></td>
+                    <td><p>David del Prado Losada - DAW2</p></td>
                     <td><a href="https://github.com/DavidelPrado" target="_blank"><img src="../img/git.png" width="50px" height="50px"></img></a></td>
                 </tr>
             </table>
